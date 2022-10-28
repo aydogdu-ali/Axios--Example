0c0ac98e31791199fcf228730cf2ca47b00fdf2b
Ali <ali.ozgur@example.com>
2022-10-28 04:06:13 +0300

    finished style

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
 import Home from "./pages/Home";
-import "./components/Dark.css"
-import { useEffect,useState } from "react";
-import {CgDarkMode}  from "react-icons/cg"
+import "./components/Dark.css";
+import { useEffect, useState } from "react";
+import { CgDarkMode } from "react-icons/cg";
 
 function App() {
+  const [theme, setTheme] = useState("light");
 
-  const [theme, setTheme] = useState('light');
-     
-    const toggleTheme = () => {
-      if (theme === 'light') {
-        setTheme('dark');
-      } else {
-        setTheme('light');
-      }
-    
-          };
-       useEffect(() => {
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+  useEffect(() => {
     document.body.className = theme;
-   
   }, [theme]);
- 
 
-  return ( 
-  <div className={`App ${theme}`}>
-  <Home />
-  <div className ="text-center">  <button className ="btn btn-danger" onClick={toggleTheme}> <CgDarkMode/>  Mode </button></div>
-   
-  </div>
-  )
+  return (
+    <div className={`App ${theme}`}>
+      <Home />
+      <div className="text-center">
+      
+        
+          <CgDarkMode
+          size={30}
+          type="button"
+           onClick={toggleTheme}/> 
+       
+      </div>
+    </div>
+  );
 }
-
 
 export default App;
